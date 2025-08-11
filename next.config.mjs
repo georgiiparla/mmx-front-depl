38ed9fc25852a364a6dcaaf9f6ae37f16ce68385
Georgii Parla <85189118+georgiiparla@users.noreply.github.com>
2025-08-11 15:42:45 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,30 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//         remotePatterns: [
+//           {
+//             protocol: 'https',
+//             hostname: 'georgiiparla.github.io',
+//             pathname: '/mmxFront/public/**', // Optionally restrict to specific paths
+//           },
+//         ],
+//       },
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'georgiiparla.github.io',
-            pathname: '/mmxFront/public/**', // Optionally restrict to specific paths
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'georgiiparla.github.io',
+        // This allows any path on the domain
+        pathname: '/**',
       },
+    ],
+  },
 };
 
 export default nextConfig;
